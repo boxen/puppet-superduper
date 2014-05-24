e3e307f5ea0c6fe96c78fcc9229bff674b59ed26
Takashi Yoshida <takashi@takashiyoshida.org>
2014-05-24 21:17:06 +0800

    Update init.pp and superduper_spec.rb

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install SuperDuper! to /Applications
+#
+#
+# Sample Usage:
+#
+class superduper {
+  package { 'SuperDuper!':
+    ensure => installed,
+    provider => 'appdmg_eula',
+    source => 'http://s3.amazonaws.com/shirtpocket/SuperDuper/SuperDuper!.dmg'
+  }
 }
--- spec/classes/superduper_spec.rb
+++ spec/classes/superduper_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
-# Rename this file to classname_spec.rb
-# Check other boxen modules for examples
-# or read http://rspec-puppet.com/tutorial/
-describe 'template' do
+
+describe 'superduper' do
   it do
-    should contain_anchor('Hello_World')
+    should contain_package('SuperDuper!').with({
+      :ensure => 'installed',
+      :provider => 'appdmg_eula'
+    })
   end
 end
